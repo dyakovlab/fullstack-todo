3117cec952e6accfe7b2831b178ded6f873bf3a0
Alexey Dyakov <dyakovlab@gmail.com>
2023-09-22 17:29:38 -0700

    some fixes

---
+++ front/src/pages/TaskDetailPage/index.ts
@@ -0,0 +1,4 @@
+import React from 'react'
+import TaskDetailPage from "./TaskDetailPage";
+
+export default React.memo(TaskDetailPage)
---
+++ front/src/pages/Tasks/index.ts
@@ -0,0 +1,4 @@
+import React from 'react'
+import Tasks from "./Tasks";
+
+export default React.memo(Tasks)
